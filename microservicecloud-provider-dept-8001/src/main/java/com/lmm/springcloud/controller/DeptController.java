e762a84256de7b8dcc4c279e66d72de7317926e8
lmm <752634866@qq.com>
2019-02-16 15:24:20 +0800

    eureka单机版的注册与发现

--- microservicecloud-provider-dept-8001/src/main/java/com/lmm/springcloud/controller/DeptController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/lmm/springcloud/controller/DeptController.java
@@ -1,21 +1,25 @@
 package com.lmm.springcloud.controller;
 
-import java.util.List;
-
+import com.lmm.springcloud.entities.Dept;
+import com.lmm.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lmm.springcloud.entities.Dept;
-import com.lmm.springcloud.service.DeptService;
+import java.util.List;
 
 @RestController
 public class DeptController {
+
     @Autowired
     private DeptService service;
+    @Autowired
+    private DiscoveryClient client;
 
     @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
     public boolean add(@RequestBody Dept dept) {
@@ -30,6 +34,19 @@ public class DeptController {
     @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
     public List<Dept> list() {
         return service.list();
+    }
+
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery() {
+        List<String> list = client.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.client;
     }
 
 }
